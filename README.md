eb574fe9ac95f6f1758cc6df3a59c929acede740
Carlos Mestre <carmesce@gmail.com>
2024-06-11 08:35:04 +0200

    Ejercicio preparado

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# 24prueba-uf1286
+# Ejercicio prueba-uf1286 2024
+
+## Enunciado
+
+No es necesario contestar por escrito, sino que se evaluará el código publicado.
+
+-	Forkee este repositorio y clónelo de forma local. Avise al docente para empezar la prueba. Cada punto se debe resolver en commit por separado y debe ser publicado. *(2 ptos)*
+-	Escribir un programa que genere una lista al azar de entre 4 y 10 elementos. Cada elemento del array deberá ser otro array con tres números naturales entre 0 y 10 también al azar. *(1,5 ptos)*
+-	Haga que eI resultado se vea en una web. Implemente un generador de tablas html, de forma que se creen dinámicamente *(0,5 ptos)*
---
+++ main.js
@@ -0,0 +1,36 @@
+function generarArrayBidimensional() {
+  function generarArray() {
+    return [0, 1, 2];
+  }
+
+  var array = [];
+  for (var i = 0; i < 10; i++) {
+    array.push(generarArray());
+  }
+  return array;
+}
+
+function mostrarArrayBidimensional(arrayBidimensional) {
+  function mostrarArray(array) {
+    array.forEach(function (elemento) {
+      var formatedElemento = (" " + elemento).slice(-2);
+      process.stdout.write(`| ${formatedElemento} `);
+    });
+    console.log("|");
+  }
+
+  function mostrarLinea() {
+    for (var i = 0; i < 15; i++) {
+      process.stdout.write("-");
+    }
+    console.log("-");
+  }
+
+  mostrarLinea();
+  arrayBidimensional.forEach(function (array) {
+    mostrarArray(array);
+    mostrarLinea();
+  });
+}
+
+mostrarArrayBidimensional(generarArrayBidimensional());
