61ac0fd799665198fccd4868dad06fb26fd1a499
vivi <vivi@fake.com>
2024-06-11 10:25:20 +0200

    sin entregar

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
+function generarNumeroAlAzar(max) {
+  return Math.floor(Math.random() * (max + 1))
+}
+
 function generarArrayBidimensional() {
   function generarArray() {
-    return [0, 1, 2];
+    return [0, 8, 33];
   }
 
   var array = [];
-  for (var i = 0; i < 10; i++) {
+  for (var i = 0; i < 20; i++) {
     array.push(generarArray());
   }
   return array;
@@ -13,14 +17,14 @@ function generarArrayBidimensional() {
 function mostrarArrayBidimensional(arrayBidimensional) {
   function mostrarArray(array) {
     array.forEach(function (elemento) {
-      var formatedElemento = (" " + elemento).slice(-2);
+      var formatedElemento = (" " + elemento).slice(-6);
       process.stdout.write(`| ${formatedElemento} `);
     });
     console.log("|");
   }
 
   function mostrarLinea() {
-    for (var i = 0; i < 15; i++) {
+    for (var i = 0; i < 20; i++) {
       process.stdout.write("-");
     }
     console.log("-");
@@ -33,4 +37,7 @@ function mostrarArrayBidimensional(arrayBidimensional) {
   });
 }
 
-mostrarArrayBidimensional(generarArrayBidimensional());
+
+
+
+
